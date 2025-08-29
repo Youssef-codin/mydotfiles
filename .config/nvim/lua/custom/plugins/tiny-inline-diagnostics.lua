bdfe1c3835189fdb1f0b42a62cd5bb1902f8c7e0
Joe-arch <youssef240100538@sut.edu.eg>
2025-08-29 20:35:36 +0300

    update plugin

--- .config/nvim/lua/custom/plugins/tiny-inline-diagnostics.lua
+++ .config/nvim/lua/custom/plugins/tiny-inline-diagnostics.lua
@@ -10,13 +10,20 @@ return {
         enabled = true,
 
         -- Always show messages on all lines for multiline diagnostics
-        always_show = false,
+        always_show = true,
 
         -- Trim whitespaces from the start/end of each line
         trim_whitespaces = false,
 
         -- Replace tabs with this many spaces in multiline diagnostics
         tabstop = 4,
+      },
+      break_line = {
+        -- Enable breaking messages after a specific length
+        enabled = true,
+
+        -- Number of characters after which to break the line
+        after = 25,
       },
     }
     vim.diagnostic.config { virtual_text = false }
