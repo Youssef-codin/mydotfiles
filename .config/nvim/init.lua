6081b65058f227d6775eebee9867317a6aec13da
Youssef-codin <youssef240100538@sut.edu.eg>
2025-12-23 16:26:34 +0200

    nm applet keybinding and better telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -439,16 +439,32 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader>sl', function()
-        local dir = vim.fn.expand '%:p:h' -- directory of current file
-        vim.cmd('cd ' .. dir) -- change working directory
-        builtin.git_files() -- open telescope git_files
-      end, { desc = '[S]earch by Git Files (local)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+      vim.keymap.set('n', '<leader>sl', function()
+        local builtin = require 'telescope.builtin'
+        local utils = require 'telescope.utils'
+
+        local ok, cwd = pcall(utils.get_git_root)
+        cwd = ok and cwd or vim.fn.expand '%:p:h'
+
+        local success = pcall(builtin.git_files, { cwd = cwd })
+        if not success then
+          builtin.find_files { cwd = cwd }
+        end
+      end, { desc = '[S]earch [L]ocal files (git → fallback)' })
+
+      vim.keymap.set('n', '<leader>sg', function()
+        local utils = require 'telescope.utils'
+
+        local ok, cwd = pcall(utils.get_git_root)
+        cwd = ok and cwd or vim.fn.expand '%:p:h'
+
+        builtin.live_grep { cwd = cwd }
+      end, { desc = '[S]earch by [G]rep (git → fallback)' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
